99bbec5db9b89af43f884dfe8041c8881e901fc1
Abdulfatah Feras <2190005989@iau.edu.sa>
2023-02-02 16:49:51 +0300

    Aburub work

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,10 +1,10 @@
 //هنا فيه العناصر اللي قابله للاستعمال بشكل متكرر, ف بدال مانستعملهم لكل widget ونكرر نفس العملية نستدعيهم وخلاص
 import 'package:flutter/material.dart';
+
 const k_Application_Title_Style = TextStyle(
     fontSize: 32,
     fontWeight: FontWeight.bold,
-    color: Color(0xFF199D8C)
-); //for landing_page + loginpage
+    color: Color(0xFF199D8C)); //for landing_page + loginpage
 const k_LandingPageButtonsTextStyle = TextStyle(
   fontSize: 15,
   fontWeight: FontWeight.bold,
@@ -21,8 +21,8 @@ const curved_backgroundDecoration = BoxDecoration(
   borderRadius:  BorderRadius.circular(),
   color: Color(0xFF199D8C),
 );*/
- const buttonColor = Color(0xFFF9DDAC);
- const k_InterfacesTitleStyle= TextStyle(
-fontSize: 26,
-fontWeight: FontWeight.bold,
+const buttonColor = Color(0xFFF9DDAC);
+const k_InterfacesTitleStyle = TextStyle(
+  fontSize: 26,
+  fontWeight: FontWeight.bold,
 ); //for most of interfaces
